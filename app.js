5233322dc03fa4b65fd79cace832b8e497166710
Thomas <thomasdelville@macbook-pro-de-thomas.home>
2021-07-18 01:35:44 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+
+setInterval(changeBG, 2000)
+
+function changeBG() {
+    const col1 = getRandomRGB();
+    const col2 = getRandomRGB();
+    const col3 = getRandomRGB();
+
+    const colorString = `rgb( ${col1}, ${col2}, ${col3})`
+    document.body.style.background = colorString;
+    document.getElementById('color').innerHTML = colorString;
+}
+
+
+function getRandomRGB (){
+    return Math.floor(Math.random() * 256);
+}
+
+
+
+
+
+
+
+
+
